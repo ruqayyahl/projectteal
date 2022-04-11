286fa4b0b240b2e4892f9b3caae32446a23c1c4f
ruqayyahlamara <r.lamara.97@gmail.com>
2022-04-11 19:47:58 +0100

    routes

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ from application import app
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
+
+
+
+
+
--- application/routes.py
+++ application/routes.py
@@ -9,4 +9,18 @@ def home():
     return render_template('home.html', title='Homepage')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/products')
+def products():
+    return render_template('products.html', title='Products')
+
+
+@app.route('/basket')
+def basket():
+    return render_template('basket.html', title='Basket')
+
 
