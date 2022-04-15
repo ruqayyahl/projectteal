ea23b94cd3fa6047e451d440a7e66af8e71bb57c
ruqayyahlamara <r.lamara.97@gmail.com>
2022-04-15 20:05:52 +0100

    routes

--- application/routes.py
+++ application/routes.py
@@ -14,13 +14,38 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route('/products')
-def products():
-    return render_template('products.html', title='Products')
+@app.route('/booking')
+def booking():
+    return render_template('booking.html', title='Booking')
 
 
-@app.route('/basket')
-def basket():
-    return render_template('basket.html', title='Basket')
+@app.route('/courses')
+def courses():
+    return render_template('courses.html', title='Courses')
+
+
+@app.route('/confirmation')
+def confirmation_message():
+    return render_template('confirmation.html', title='Confirmation Message')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact Us')
+
+
+@app.route('/instructors')
+def instructors():
+    return render_template('instructors.html', title='Instructors')
+
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html', title='FAQs')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', title='Confirmed Bookings')
 
 
